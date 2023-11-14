480d319c75fb67bacb91a7dd24b143d6f9b7c46d
Đàm Việt Cường <cuongdv@overate-vntech.com>
2023-11-15 02:06:49 +0700

    ininital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
-
 function App() {
+  const [advice, setAdvice] = useState('');
+
+  useEffect(() => {
+    console.log('hello');
+    fetchAdvice();
+  }, []);
+
+  const fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        console.log(response.data);
+        setAdvice(response.data.slip.advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+        <div className='card'>
+            <h1 className='heading'>{advice}</h1>
+            <button className='button' onClick={fetchAdvice}>
+              <span>GIVE ME ADVICE!</span>
+            </button>
+        </div>
     </div>
   );
 }
